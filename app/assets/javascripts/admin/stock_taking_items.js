4c721febad8b6930816bc299bc782527d0e7e5a9
Jimmy Liang <lyh@yazotech.com>
2013-06-26 11:33:39 +0800

    ui

--- app/assets/javascripts/admin/stock_taking_items.js
+++
@@ -1,3 +0,0 @@
-function stock_taking_items(){
-
-}
---
+++ app/assets/javascripts/admin/stock_taking_sum.js
@@ -0,0 +1,24 @@
+function stock_taking_sum(){
+  $('table.sumary').each(function(){
+    $(this).find('tfoot tr td:nth-child(2)').text((sum_table_column(this)));
+    $(this).find('tfoot tr td:nth-child(3)').text((sum_table_field(this)));
+  });
+
+
+}
+
+function sum_table_column(table) {
+  var tot = 0;
+  $(table).find('tbody tr td:nth-child(4)').each(function() {
+    tot += parseInt($(this).text());
+  });
+  return tot;
+}
+
+function sum_table_field(table, columnIndex) {
+  var tot = 0;
+  $(table).find('input').each(function() {
+    tot += parseInt($(this).val());
+  });
+  return tot;
+}
