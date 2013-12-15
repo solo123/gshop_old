d0896c7b78c7791bd3e94c565a19598826f35c19
Jimmy Liang <lyh@yazotech.com>
2013-12-15 22:49:32 +0800

    customer sales sheet

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ActiveRecord::Base
 #  has_and_belongs_to_many :manufactories
+  has_many :sales_sheets
   def customer_phone_name
     "#{self.phone} #{self.name}#{self.title}"
   end
