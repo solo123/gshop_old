1736e656b02c113d8c32764d45e601e46a1a2bed
Jimmy Liang <jimmy.liang@gmail.com>
2014-06-17 20:35:49 +0800

    default scope

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -5,7 +5,7 @@ class Guitar < ActiveRecord::Base
   has_many :godown_items, :as => :product_data
   has_many :stocks, :as => :product_data
   has_many :serial_products, :as => :product_data
-  default_scope order('guitar_type, short_code')
+  default_scope { order('guitar_type, short_code') }
   def full_name
     "#{brand} #{model}"
   end
--- app/models/stock_taking_item.rb
+++ app/models/stock_taking_item.rb
@@ -3,5 +3,5 @@ class StockTakingItem < ActiveRecord::Base
   belongs_to :stock_taking
   belongs_to :product_data, :polymorphic => true
 
-  default_scope order('catalog, full_name')
+  default_scope { order('catalog, full_name') }
 end
