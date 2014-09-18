612f115b1e0a7a01af9e74c56da73447f5524256
Jimmy Liang <jimmy.liang@gmail.com>
2014-09-18 13:53:21 +0800

    product

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,6 +1,9 @@
 class CatalogsController < ApplicationController
   def show
     @catalog = Product.select('catalog, count(*) as cnt').order(:catalog).group(:catalog)
+    if params[:cat]
+      @collection = Product.where(catalog: params[:cat])
+    end
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   has_one :product_price
-  default_scope { order 'catalog, brand, model' }
+  default_scope { order 'catalog, status desc, full_name' }
   scope :search_by_name, -> (q) { where(['full_name like ? or catalog like ?', "%#{q}%"]) }
 
   def product_short_name_fullname
